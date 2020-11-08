3ba0a16ca34f0168e91769dde91ccf209a4ab15b
marcialwushu <marcialwushu@gmail.com>
2020-11-08 18:35:18 -0300

    get All images and save in ArrayList

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+
 
 (async () => {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
-	await page.goto('https:example.com');
-	await page.screenshot({path: 'example.png' });
+	await page.goto('https://instagram.com/rocketseat_oficial');
+	
+	const imgList = await page.evaluate(() => {
+
+		const nodeList = document.querySelectorAll('article img')
+
+		const imgArray = [...nodeList ]
+
+		const imgList = imgArray.map( ({src}) => ({
+			src
+		  }))	
+
+	return imgList
+
+	});
+
+	fs.writeFile('instagram.json', JSON.stringify(imgList, null, 2), err => {
+	if(err) throw new Error('somethig went wrong')
+
+	console.log('well done!')
+})
 
 	await browser.close();
 })();
